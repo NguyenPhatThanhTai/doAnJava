f4fe3127f9abcb0e9138311abde36d3f23eaf938
NguyenPhatThanhTai <57514749+NguyenPhatThanhTai@users.noreply.github.com>
2021-03-10 14:50:55 +0700

    Update #2
    Show and Hide form

--- doAnJava/src/viewForm/Controller/Controller.java
+++ doAnJava/src/viewForm/Controller/Controller.java
@@ -1,17 +1,25 @@
-package viewForm;
+package viewForm.Controller;
 
 import DAO.doDao;
 import com.sun.jnlp.ApiDialog;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import javax.swing.*;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Controller implements Initializable {
     @FXML
@@ -29,6 +37,18 @@ public class Controller implements Initializable {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setHeaderText("Đăng nhập thành công!");
                 alert.showAndWait();
+
+                try {
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../View/Main.fxml"));
+                    Parent root1 = (Parent) fxmlLoader.load();
+                    Stage stage = new Stage();
+                    stage.setTitle("Main form");
+                    stage.setScene(new Scene(root1));
+                    stage.show();
+                    ((Node)(e.getSource())).getScene().getWindow().hide();
+                } catch (Exception ioException) {
+                    ioException.printStackTrace();
+                }
             }
             else {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -36,6 +56,11 @@ public class Controller implements Initializable {
                 alert.showAndWait();
             }
         }
+    }
+
+    public void openNewWindows(ActionEvent e){
+        txtTaiKhoan.setText("");
+        txtMatKhau.setText("");
     }
 
     @Override
